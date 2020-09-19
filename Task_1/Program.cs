e27b9e6a9c7b84c760ab8862bceb174983eb3402
Євгеній Грищук <hryshchuk.i@donnu.edu.ua>
2020-09-19 15:18:00 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Привіт,ДонНУ");
+        }
+    }
+}
